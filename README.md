1ee9fec24b8ac30bc3cc5088451b492d3f4c4fbd
yutaeguchic <yutaeguchic@gmail.com>
2020-02-03 03:39:49 +0900

    検品環境の追加

--- README.md
+++ README.md
@@ -7,17 +7,42 @@ npm install
 
 ### Compiles and hot-reloads for development
 ```
+// development project
+npm run dev_serve
+
+// production project
 npm run serve
 ```
 
 ### Compiles and minifies for production
 ```
+// development project
+npm run dev_build
+
+// production project
 npm run build
 ```
 
 ### Lints and fixes files
 ```
 npm run lint
+```
+
+### Style(stylus) build
+```
+gulp
+```
+
+### Deploy
+```
+// development project
+firebase use dev
+firebase deploy --project dev
+
+// production project
+firebase use prod
+firebase deploy --project prod
+
 ```
 
 ### Customize configuration
--- src/main.js
+++ src/main.js
@@ -8,17 +8,19 @@ import './assets/css/style.css'
 Vue.config.productionTip = false
 Vue.use(firestorePlugin)
 const firebaseConfig = {
-  apiKey: "AIzaSyDjK08oTr8azL-AxP1aqCqnwzCgM-jv4dA",
-  authDomain: "nanikiru-4db40.firebaseapp.com",
-  databaseURL: "https://nanikiru-4db40.firebaseio.com",
-  projectId: "nanikiru-4db40",
-  storageBucket: "nanikiru-4db40.appspot.com",
-  messagingSenderId: "529523796832",
-  appId: "1:529523796832:web:4122e63a0157702cc8cc05",
-  measurementId: "G-ZQQGPWPRWL"
+  apiKey: process.env.VUE_APP_FB_API_KEY,
+  authDomain: process.env.VUE_APP_FB_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FB_DATABASE_URL,
+  projectId: process.env.VUE_APP_FB_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FB_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FB_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FB_APP_ID,
+  measurementId: process.env.VUE_APP_FB_MEASUREMENT_ID
 }
 firebase.initializeApp(firebaseConfig)
 firebase.analytics()
+
+console.log(firebaseConfig)
 
 new Vue({
   router,
